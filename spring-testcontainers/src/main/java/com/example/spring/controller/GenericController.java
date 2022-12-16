d180a1ec628f04b42ee98837684bc5f052043618
Kalathil Ajay Menon <ajay.java.dev@gmail.com>
2022-12-15 18:44:18 -0600

    base

---
+++ spring-testcontainers/src/main/java/com/example/spring/controller/GenericController.java
@@ -0,0 +1,15 @@
+package com.example.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenericController {
+
+    @GetMapping("/messages")
+    public String getMessage() {
+        return "Hello from Docker!";
+    }
+
+
+}
