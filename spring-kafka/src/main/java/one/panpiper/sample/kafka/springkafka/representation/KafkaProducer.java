6427697c6b32efbf6a8f678444ef9aa5fcd7e94c
Kalathil Ajay Menon <ajay.java.dev@gmail.com>
2023-03-22 16:16:25 -0500

    logging

--- spring-kafka/src/main/java/one/panpiper/sample/kafka/springkafka/representation/KafkaProducer.java
+++ spring-kafka/src/main/java/one/panpiper/sample/kafka/springkafka/representation/KafkaProducer.java
@@ -2,6 +2,7 @@ package one.panpiper.sample.kafka.springkafka.representation;
 
 import com.github.javafaker.Faker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationStartupAware;
 import org.springframework.context.event.EventListener;
@@ -11,12 +12,14 @@ import reactor.core.publisher.Flux;
 import reactor.util.function.Tuple2;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaProducer {
 
     private final KafkaTemplate<Integer, String> template;
@@ -33,6 +36,7 @@ public class KafkaProducer {
 
         Flux.zip(interval, quotes)
                 .map((Function<Tuple2<Long, String>, Object>) objects -> {
+                    log.debug("Event object : {}, value: {}, sent on: {}",objects.getT1(), objects.getT2(), Instant.now());
                     return template.send("ENTERPRISE.PAN.PIPER.HARRY.EVENTS", faker.random().nextInt(42), objects.getT2());
                 })
                 .blockLast();
